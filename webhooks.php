6d6d13163fd36550ed6f787cc2afb2377aaf5030
max301 <b.301@hotmail.com>
2018-06-18 13:04:56 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'y+G4+IONObnFNf+jWBtTZ+n8vmRfSMDiEQl+ishCzET5gcMc5DOpb3+5Ju0Y5zalj+VNbzQpRBeeRWXp9ZMYs/AuTBiOVH900cbUe7ykFJS1A+0SxWv9GZzbMvmT9KrofDpwz0jSCamihP18qdcGCAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
